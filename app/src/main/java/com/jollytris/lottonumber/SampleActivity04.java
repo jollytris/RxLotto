ee752fa6d384a99c717b3297426c63aaf30bb939
zic325 <zic325@zic325ui-MacBook-Pro.local>
2017-04-13 00:24:04 +0900

    commit

--- app/src/main/java/com/jollytris/lottonumber/SampleActivity04.java
+++ app/src/main/java/com/jollytris/lottonumber/SampleActivity04.java
@@ -1,13 +1,44 @@
 package com.jollytris.lottonumber;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
 
 public class SampleActivity04 extends AppCompatActivity {
+
+    private PublishSubject<Integer> subject;
+    private int drawNo = 739;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample04);
+        textView = (TextView) findViewById(R.id.textView);
+        RxView.clicks(findViewById(R.id.button))
+                .subscribe(e -> {
+                    subject.onNext(drawNo--);
+                });
+
+        subject = PublishSubject.create();
+        subject.subscribe(no -> {
+            getWinNumber(no)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .map(lotto -> lotto.toString() + "\n")
+                    .subscribe(textView::append);
+        });
+    }
+
+
+    private Observable<Lotto> getWinNumber(int number) {
+        return LottoRequester.getInstance().api().getWinNumber("getLottoNumber", number);
     }
 }
